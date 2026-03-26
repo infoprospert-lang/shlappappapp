935afc69ba608eae4bdcc96e24b1801237a6e6c6
Pawel <infoprospert@gmail.com>
2026-03-26 10:18:57 +0100

    Configure Railway deployment

--- server.ts
+++ server.ts
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = parseInt(process.env.PORT || "3000");
 
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
